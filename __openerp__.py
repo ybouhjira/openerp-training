86f0e2ec6e7df192c1c4ab9c184a1d56dfccf202
Youssef Bouhjira <b.youssef91@gmail.com>
2014-07-31 14:05:37 +0000

    __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Training',
+    'version': '0.1',
+    'category': 'Training',
+    'description': """
+    Hello world!
+    """,
+    'author': 'HoomamLabs',
+    'website': 'http://Hoomamlabs.com',
+    #'images': ['images/internal_sequence_number.jpeg'],
+    'depends': ['base'],
+    #'data': [],
+    #'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
